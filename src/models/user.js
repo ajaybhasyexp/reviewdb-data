8341d4cd07e1fcba3b4425f4c14c2da688fd8686
ajaybhasy-experion <ajay.bhasy@experionglobal.com>
2020-02-25 09:34:41 +0530

    Added model and service for user

---
+++ src/models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    photoUrl: {
+        type: String
+    },
+    socialId: { type: String, required: true },
+    email: String,
+    provider: { type: String, required: true }
+
+});
+
+module.exports = mongoose.model('User', userSchema);
